6f3e563a6c8b30e7e31a5d2b89a2818756855273
hari <hari@192.168.43.46>
2020-10-18 13:02:06 +0530

    Test Class Files Committing

---
+++ src/test/java/com/nit/hari/SpringBoot2MultipleDBsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit.hari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2MultipleDBsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
